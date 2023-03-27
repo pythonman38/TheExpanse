582a2711960591d0bedd9e7868944cc7d9d637e5
Michael Walker <pythonk@email.phoenix.edu>
2023-03-26 23:07:51 -0400

    Home Screen - Section 3
    Setup a backend server using express which feeds into the frontend webpage. Added routing so each product could have it's own detail page. Used a few of the React hooks to set states when fetching products from the API. In the next commit I will add react bootstrap to format the home screen a little better.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">The Expanse</a>
-      </header>
-      <main>
-        list products
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">The Expanse</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen/>}/>
+            <Route path="/" element={<HomeScreen/>}/>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
