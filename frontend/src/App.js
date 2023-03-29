f8641d70cf198207874cc052cf2d27045283063c
Michael Walker <pythonk@email.phoenix.edu>
2023-03-29 12:16:17 -0400

    Cart Screen - Section 6
    Created React context to save shopping cart items when the 'Add to Cart' button is clicked. Added an id element to each product in the data table. Adding a unique item prevents the shopping cart from having duplicate items (and instead increases quantities). Created a new API to return products using the new id. Added new route for CartScreen to display the contents of the shopping cart as well as a button for checking out. Added navigation to direct user to the CartScreen after adding an item to the cart. Finally added some images to add, subtract and remove quantities from the shopping cart.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
-import { Container, Navbar } from "react-bootstrap";
+import { Badge, Container, Nav, Navbar } from "react-bootstrap";
 import { LinkContainer } from 'react-router-bootstrap'
+import { useContext } from "react";
+import { Store } from "./Store";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
@@ -14,6 +19,16 @@ function App() {
               <LinkContainer to="/">
                 <Navbar.Brand>The Expanse</Navbar.Brand>
               </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart {
+                  cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
         </header>
@@ -21,6 +36,7 @@ function App() {
           <Container className="mt-3">
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen/>}/>
+              <Route path="/cart" element={<CartScreen/>}/>
               <Route path="/" element={<HomeScreen/>}/>
             </Routes>
           </Container>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
-import { useParams } from "react-router-dom";
-import { useEffect, useReducer } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useContext, useEffect, useReducer } from "react";
 import axios from "axios";
 import { Badge, Button, Card, Col, ListGroup, Row } from "react-bootstrap";
 import Rating from "../components/Rating";
@@ -7,6 +7,7 @@ import { Helmet } from "react-helmet-async";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 import { getError } from "../utilities";
+import { Store } from "../Store";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -22,6 +23,7 @@ const reducer = (state, action) => {
 };
 
 function ProductScreen() {
+  const navigate = useNavigate();
   const params = useParams();
   const { slug } = params;
   const [{ loading, error, product }, dispatch] = useReducer(reducer, {
@@ -41,6 +43,23 @@ function ProductScreen() {
     };
     fetchData();
   }, [slug]);
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart } = state;
+  const addToCartHandler = async () => {
+    const existItem = cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock!');
+      return;
+    }
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...product, quantity }
+    });
+    navigate('/cart');
+  };
 
   return loading ? (
     <LoadingBox/>
@@ -93,11 +112,10 @@ function ProductScreen() {
                 {product.countInStock > 0 && (
                   <ListGroup.Item>
                     <div className="d-grid">
-                      <Button variant="primary">Add to Cart</Button>
+                      <Button onClick={addToCartHandler} variant="primary">Add to Cart</Button>
                     </div>
                   </ListGroup.Item>
                 )}
-
               </ListGroup>
             </Card.Body>
           </Card>
